0cc756d718f2c9a9bb659061d8b4514ac5c5d443
Bhatt Nisarg <nmbhatt2001@gmail.com>
2022-02-23 17:41:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/uidemo/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.uidemo;
+
+import androidx.annotation.DrawableRes;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
